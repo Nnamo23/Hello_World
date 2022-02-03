a227fcfd9afc95ca584583daae3cd0203f2654a1
Nnamdi Afamefuna Ndili <61285757+Nnams23@users.noreply.github.com>
2022-02-03 20:06:27 +0100

    Initial commit

---
+++ Hello, World!.py
@@ -0,0 +1,13 @@
+def hello_world():
+    print("This is a user-test")
+    user_input = input("How many fingers am I holding up: ")
+
+    if user_input == "4":
+        print("\nWell Done")
+
+    else:
+        print("\nNope, Try Again")
+
+print("Hello, World! \n")
+hello_world()
+
